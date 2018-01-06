327110173f0f0ee1f93471ef4bd1c9acada513e4
Bibhas Bhattacharya <bibhas2@gmail.com>
2018-01-05 19:39:02 -0500

    Save training weights

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import sys
 
 def weight_variable(shape, name):
     initial = tf.truncated_normal(shape, stddev=0.001)
@@ -34,7 +35,10 @@ class RecommenderModel(object):
         self.r_hat = tf.reduce_sum(tf.multiply(self.U_embed, self.V_embed), reduction_indices=1)
         self.r_hat = tf.add(self.r_hat, self.U_bias_embed)
         self.r_hat = tf.add(self.r_hat, self.V_bias_embed)
-        self.r_hat = tf.add(self.r_hat, tf.reduce_mean(self.r))
+        # self.r_hat = tf.add(self.r_hat, tf.reduce_mean(self.r))
+        self.r_predict = tf.add(
+            tf.matmul(self.U, tf.transpose(self.V)),
+            tf.matmul(tf.reshape(self.U_bias, [self.num_users, 1]), tf.transpose(tf.reshape(self.V_bias, [self.num_items, 1]))))
 
         self.l2_loss = tf.nn.l2_loss(tf.subtract(self.r, self.r_hat))
         self.reg = tf.add(tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.U)), tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.V)))
@@ -49,10 +53,35 @@ class RecommenderModel(object):
 
         return session.run(self.l2_loss, feed_dict)
     
+    def recommend(self, session, user_idx, result_count):
+        _, item_list = session.run(tf.nn.top_k(self.r_predict[user_idx], result_count))
+        return item_list
+
     def predict(self, session, u_idx, v_idx):
-        feed_dict = {self.u_idx:u_idx, self.v_idx:v_idx}
+        feed_dict = {self.u_idx:[u_idx], self.v_idx:[v_idx]}
 
         return session.run(self.r_hat, feed_dict)
+
+    def get_similar_products(self, session, item_index, result_count):
+        #Get the normalized product parameters
+        V_normalized = session.run(tf.nn.l2_normalize(self.V, 1))
+
+        #Get the parameters for this product
+        this_params_normalized = V_normalized[item_index]
+
+        cos_similarity = tf.reduce_sum(tf.multiply(V_normalized,this_params_normalized), 1)
+        top_similar = tf.nn.top_k(cos_similarity, result_count)
+        score_list, idx_list = session.run(top_similar)
+
+        return idx_list
+    
+    def save(self, session):
+        saver = tf.train.Saver()
+        saver.save(session, "./model.ckpt")
+
+    def restore(self, session):
+        saver = tf.train.Saver()
+        saver.restore(session, "./model.ckpt")
 
 class MovieLensDataLoader(object):
     def __init__(self):
@@ -100,7 +129,7 @@ class MovieLensDataLoader(object):
         return (user_idx, item_idx, rating)
 
 
-def main():
+def train():
     with MovieLensDataLoader() as loader:
         print "Users:", loader.num_users
         print "Items:", loader.num_items
@@ -112,19 +141,54 @@ def main():
             init_op = tf.global_variables_initializer()
             session.run(init_op)
 
-            for step in range(0, 5000):
+            for step in range(0, 3500):
                 train_user_idx, train_item_idx, train_rating = loader.load_next_batch(200)
                 loss = model.train_batch(session, train_user_idx, train_item_idx, train_rating)
                 
                 if step % 10 == 0:
                     print "Step:", step, "Loss:", loss
+            model.save(session)
 
-            #Run prediction
-            user_idx, item_idx, rating_real = loader.load_next_batch(10)
+def recommend(user_idx):
+    with MovieLensDataLoader() as loader:
+        with tf.Session() as session:
+            model = RecommenderModel(loader.num_users, loader.num_items, num_features=2000)
 
-            rating_predicted = model.predict(session, user_idx, item_idx)
+            model.restore(session)
 
-            print "Real:", rating_real
-            print "Prediction:", rating_predicted
+            recommended_items = model.recommend(session, user_idx, 5)
 
-main()
+            print "Recommended:", recommended_items
+
+def predict_rating(user_idx, item_idx):
+    with MovieLensDataLoader() as loader:
+        with tf.Session() as session:
+            model = RecommenderModel(loader.num_users, loader.num_items, num_features=2000)
+
+            model.restore(session)
+
+            rating = model.predict(session, user_idx, item_idx)
+
+            print "Predicted rating:", rating
+
+def similar_items(item_idx):
+    with MovieLensDataLoader() as loader:
+        with tf.Session() as session:
+            model = RecommenderModel(loader.num_users, loader.num_items, num_features=2000)
+
+            model.restore(session)
+
+            index_list = model.get_similar_products(session, item_idx, 5)
+
+            print index_list
+
+if len(sys.argv) == 1:
+    print "Usage: [--train] [--recommend user_index] [--similar item_index] [--predict user_index item_index]"
+elif sys.argv[1] == "--train":
+    train()
+elif sys.argv[1] == "--recommend":
+    recommend(int(sys.argv[2]))
+elif sys.argv[1] == "--similar":
+    similar_items(int(sys.argv[2]))    
+elif sys.argv[1] == "--predict":
+    predict_rating(int(sys.argv[2]), int(sys.argv[3]))    
